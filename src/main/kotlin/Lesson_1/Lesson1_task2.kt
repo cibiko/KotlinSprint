0dcc6dffb666ac6fb0f53e205aecccc73d49408c
saygidmagomedoff <Saygidmagomedoff@gmail.com>
2025-10-25 20:09:42 +0300

    KS-1-2

--- src/main/kotlin/Lesson_1/Lesson1_task2.kt
+++ src/main/kotlin/Lesson_1/Lesson1_task2.kt
@@ -1,5 +1,12 @@
 package org.example.Lesson_1
 
-fun main(){
-    println("Тестовое решение ко второй задаче")
+fun main() {
+    val sumorder: Int = 75
+    val gratitude: String = "Благодарим Вас за покупку "
+//  val storage = 2000
+    val storage = 1999
+
+    println(storage)
+    println(sumorder)
+    println(gratitude)
 }

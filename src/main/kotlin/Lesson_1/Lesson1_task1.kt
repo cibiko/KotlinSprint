64dce5383dd406ee54c69936b38700451edc06a8
saygidmagomedoff <Saygidmagomedoff@gmail.com>
2025-10-25 21:38:30 +0300

    KS-1-1

--- src/main/kotlin/Lesson_1/Lesson1_task1.kt
+++ src/main/kotlin/Lesson_1/Lesson1_task1.kt
@@ -2,6 +2,6 @@ package org.example.Lesson_1
 
 fun main() {
     val ordersCount: Int = 75
-    val textWithThanks: String = "Благодарим Вас за покупку "
+    val textWithThanks: String = "Благодарим Вас за покупку"
 
 }

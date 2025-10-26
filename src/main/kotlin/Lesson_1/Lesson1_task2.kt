dc895adc3e9bfc69cfa3b7e957432f0829eff2fa
saygidmagomedoff <Saygidmagomedoff@gmail.com>
2025-10-27 00:12:13 +0300

    KS-1-2

--- src/main/kotlin/Lesson_1/Lesson1_task2.kt
+++ src/main/kotlin/Lesson_1/Lesson1_task2.kt
@@ -3,12 +3,15 @@ package org.example.Lesson_1
 fun main() {
     val orderCount: Int = 75
     val textWithThanks: String = "Благодарим Вас за покупку "
-    var numberOfWorkers = 2000
-    numberOfWorkers = 1999
 
     println(orderCount)
     println(textWithThanks)
-//  println(numberOfWorkers)
+
+    var numberOfWorkers = 2000
+    //  println(numberOfWorkers)
+
+    numberOfWorkers = 1999
     println(numberOfWorkers)
+
 
 }

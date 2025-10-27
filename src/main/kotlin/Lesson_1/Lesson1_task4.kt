9ea95a9e6ed494c5ac8c792a9fd47a167b9b68b8
saygidmagomedoff <Saygidmagomedoff@gmail.com>
2025-10-27 16:53:35 +0300

    KS-1-4

---
+++ src/main/kotlin/Lesson_1/Lesson1_task4.kt
@@ -0,0 +1,17 @@
+package org.example.Lesson_1
+
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfday: Double = 0.075
+    val seconds: Int = 6480
+    val partOfYear: Double =  2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfday)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}

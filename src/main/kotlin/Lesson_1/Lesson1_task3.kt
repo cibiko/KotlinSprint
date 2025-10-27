84ac721529483a576f8b59c0d840454492ef027c
saygidmagomedoff <Saygidmagomedoff@gmail.com>
2025-10-27 19:24:05 +0300

    KS-1-3

--- src/main/kotlin/Lesson_1/Lesson1_task3.kt
+++ src/main/kotlin/Lesson_1/Lesson1_task3.kt
@@ -2,15 +2,15 @@ package org.example.Lesson_1
 
 fun main() {
     val yearOfFlight = 1991
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
 
     println(yearOfFlight)
     println(hour)
     println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     print(hour)
     print(":")

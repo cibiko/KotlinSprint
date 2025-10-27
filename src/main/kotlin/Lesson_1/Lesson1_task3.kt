8c250fff0403deb90a6fc5255e77dcb45cb28940
saygidmagomedoff <Saygidmagomedoff@gmail.com>
2025-10-27 16:14:24 +0300

    KS-1-3

--- src/main/kotlin/Lesson_1/Lesson1_task3.kt
+++ src/main/kotlin/Lesson_1/Lesson1_task3.kt
@@ -1,5 +1,19 @@
 package org.example.Lesson_1
 
-fun main(){
-    println("Тестовое решение к третьей  задачее")
+fun main() {
+    val yearOfFlight = 1991
+    var hour = 9
+    var minute = 7
+
+    println(yearOfFlight)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")
+    print(minute)
+
 }

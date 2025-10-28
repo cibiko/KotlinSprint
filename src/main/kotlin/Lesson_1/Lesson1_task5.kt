c2fdf53499564270fa3c1f867981f648f1a9f47f
saygidmagomedoff <Saygidmagomedoff@gmail.com>
2025-10-29 00:33:43 +0300

    KS-1-5

--- src/main/kotlin/Lesson_1/Lesson1_task5.kt
+++ src/main/kotlin/Lesson_1/Lesson1_task5.kt
@@ -1,12 +1,12 @@
 package org.example.Lesson_1
 
-const val TOTAL_SECONDS = 6480
 const val SECONDS_IN_HOUR = 3600
 const val SECONDS_IN_MINUTE = 60
 
 fun main() {
-    val hourOfFlight = TOTAL_SECONDS / SECONDS_IN_HOUR
-    val remainingTotalSeconds = TOTAL_SECONDS % SECONDS_IN_HOUR
+    val totalSeconds= 6480
+    val hourOfFlight = totalSeconds / SECONDS_IN_HOUR
+    val remainingTotalSeconds = totalSeconds % SECONDS_IN_HOUR
     val minuteOfFlight = remainingTotalSeconds / SECONDS_IN_MINUTE
     val secondsOfFlight = remainingTotalSeconds % SECONDS_IN_MINUTE
 

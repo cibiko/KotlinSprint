73197505309ef2825746006abad562cc368e09cb
saygidmagomedoff <Saygidmagomedoff@gmail.com>
2025-10-31 00:20:41 +0300

    KS-2-4

--- src/main/kotlin/Lesson_2/KS-2-4.kt
+++ src/main/kotlin/Lesson_2/KS-2-4.kt
@@ -1,13 +1,14 @@
 package org.example.Lesson_2
 
-const val TWENTY_PER_CENT = 0.20
+const val MAX_PER_CENT = 100
+const val TWENTY_PER_CENT = 20
 
 fun main() {
     val crystallineIronOre = 7
     val ironOre = 11
 
-    val buffCrystallineIronOre = (crystallineIronOre * TWENTY_PER_CENT).toInt()
-    val buffIronOre = (ironOre * TWENTY_PER_CENT).toInt()
+    val buffCrystallineIronOre = (crystallineIronOre / MAX_PER_CENT.toFloat() * TWENTY_PER_CENT).toInt()
+    val buffIronOre = (ironOre / MAX_PER_CENT.toFloat() * TWENTY_PER_CENT).toInt()
 
     println("Бонусные кристалические руды: $buffCrystallineIronOre")
     println("Бонусные железные руды: $buffIronOre")
